2bc18e477bbcfff278a228d9d0b55e42f4c632f2
minhair <minhairtech@gmail.com>
2022-03-23 15:38:46 +0700

    Fix bug

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const
     app = express().use(bodyParser.json()); // creates express http server
 
 // Sets server port and logs message on success
-app.listen(process.env.PORT || 80, () => console.log('webhook is listening'));
+app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
 
 // Creates the endpoint for our webhook 
 app.post('/webhook', (req, res) => {
@@ -37,7 +37,7 @@ app.post('/webhook', (req, res) => {
 
 });
 
-console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+console.log("bb")
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req, res) => {
